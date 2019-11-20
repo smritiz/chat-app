da4bcdb705619419bae978732049e4c43103fe62
Smriti Bauasaheb Jha <smriti.jha@incedoinc.com>
2019-11-20 17:16:01 +0530

    first message

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const PORT = PROCESS.env.PORT ||9090
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname+"/socket-client.html")
+})
+
+io.on("connection", client => {
+    console.log("Client connected");
+    client.emit("acknowledge", {data : "Connected Now"})
+
+    client.on("MsgToServer", ({chatterName, message}) => {
+        console.log("Client Says : " + message);
+        client.emit("MsgToClient", {chatterName : 'Me', message});
+        client.broadcast.emit("MsgToClient", {chatterName, message});
+    })
+
+    client.on("disconnect", () => {
+        console.log("Client disconnected!");
+    })
+})
+
+server.listen(9090, () => {
+    console.log("Socket server started at port :" + PORT)
+})
